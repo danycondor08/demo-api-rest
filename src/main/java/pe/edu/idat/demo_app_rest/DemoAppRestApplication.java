f2de0512c612fe4967ce65662efe5e531d4fd65f
danycondor08 <danycristophercondorsolano@gmail.com>
2025-02-20 08:06:45 -0500

    initial commit

---
+++ src/main/java/pe/edu/idat/demo_app_rest/DemoAppRestApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_app_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/demo_app_rest/DemoAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_app_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
